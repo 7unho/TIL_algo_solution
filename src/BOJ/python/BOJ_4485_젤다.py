4664554ddfb39155472da0b5de5149096e3f7724
april2nds <rlawnsgh8395@naver.com>
2022-11-08 23:32:24 +0900

    [UPDATE] BOJ_4485 solved

--- src/BOJ/python/BOJ_4485_젤다.py
+++ src/BOJ/python/BOJ_4485_젤다.py
@@ -1,42 +1,42 @@
-import heapq
-import sys
-
+import sys, heapq
 input = sys.stdin.readline
-INF = int(1e9)
-answers = list()
 
+INF = int(1e9)
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+TC = 1
 while True:
     N = int(input())
-    tc = 1
-    if N == 0:
-        break
+
+    if N == 0: break
 
     graph = [list(map(int, input().split())) for _ in range(N)]
-    dp = [[INF] * (N) for _ in range(N)]
+    dp = [[INF] * N for _ in range(N)]
 
-    dx = [0, 0, 1, -1]
-    dy = [1, -1, 0, 0]
 
     dp[0][0] = graph[0][0]
-    queue = list()
-    heapq.heappush(queue, (0, 0, dp[0][0]))
-    while queue:
-        x, y, dist = heapq.heappop(queue)
-        if dp[x][y] < dist:
-            continue
+
+    q = []
+    heapq.heappush(q, (graph[0][0], (0, 0)))
+
+    while q:
+        dist, cur = heapq.heappop(q)
+        x, y = cur
+
+        if dist > dp[x][y]: continue
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if nx < 0 or ny < 0 or nx >= N or ny >= N:
-                continue
+
+            if nx < 0 or ny < 0 or nx >= N or ny >= N: continue
+
             cost = dist + graph[nx][ny]
+
             if cost < dp[nx][ny]:
                 dp[nx][ny] = cost
-                heapq.heappush(queue, (nx, ny, cost))
+                heapq.heappush(q, (cost, (nx, ny)))
 
-
-    answers.append(dp[N - 1][N - 1])
-
-
-for tc in range(len(answers)):
-    print(f"Problem {tc + 1}: {answers[tc]}")
+    print(f"Problem {TC}: {dp[N - 1][N - 1]}")
+    TC += 1
