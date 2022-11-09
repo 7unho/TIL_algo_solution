2b2fa98a51015ccb9c4c8ab147205c87a757d457
april2nds <rlawnsgh8395@naver.com>
2022-11-09 23:34:17 +0900

    [ADD] SWE_1249_등산로조성 solved

--- src/SWEA/python/SWE_1949_등산로조성.py
+++ src/SWEA/python/SWE_1949_등산로조성.py
@@ -1,58 +1,66 @@
+import sys
+input = sys.stdin.readline
+
 for tc in range(1, int(input()) + 1):
+    # N : N*N graph, K : 깎을 수 있는 높이.
     N, K = map(int, input().split())
-    answer = 0
-    points = []
-    MAX = 0
+    graph = [list(map(int, input().split())) for _ in range(N)]
 
-    dx = [0, 0, 1, 1]
+    dx = [0, 0, 1, -1]
     dy = [1, -1, 0, 0]
+    MAX = 0
+    flag = 1
+    answer = 0
 
-    graph = []
+    points = []
+
+    # 최댓값 찾기
     for i in range(N):
-        graph.append(list(map(int, input().split())))
         MAX = max(MAX, max(graph[i]))
 
+    # 최댓값인 곳의 좌표 points에 추가
     for i in range(N):
         for j in range(N):
-            if graph[i][j] == MAX: points.append([i, j, 1])
+            if graph[i][j] == MAX: points.append((i, j))
 
-    def dfs(point, visited, dist):
-        global answer
-        x = point[0]
-        y = point[1]
-        flag = point[2]
+
+    visited = [[False] * N for _ in range(N)]
+    
+    # 탐색.
+    def dfs(x, y, dist):
+        global answer, flag
         answer = max(answer, dist)
 
-        visited[x][y] = True
 
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if nx < 0 or ny < 0 or nx >= N or ny >= N:
+            if nx < 0 or ny < 0 or nx >= N or ny >= N or visited[nx][ny]:
                 continue
 
-            if visited[nx][ny]: continue
-        
+            # 횟수가 남아있으면서, 현재 높이보다 크거나 같다면.
             if flag == 1 and graph[nx][ny] >= graph[x][y]:
-                for i in range(1, K + 1):
-                    graph[nx][ny] -= i
+                # K 감소하면서 최댓값으로 깎아내리기..
+                for depth in range(1, K + 1):
                     flag = 0
-                    if graph[x][y] > graph[nx][ny]:
+                    if graph[nx][ny] - depth < graph[x][y]:
                         visited[nx][ny] = True
-                        dfs([nx, ny, flag], visited, dist + 1)
+                        graph[nx][ny] -= depth
+                        dfs(nx, ny, dist + 1)
+                        graph[nx][ny] += depth
                         visited[nx][ny] = False
                     flag = 1
-                    graph[nx][ny] += i
+            # 현재 높이보다 작다면
             elif graph[nx][ny] < graph[x][y]:
                 visited[nx][ny] = True
-                dfs([nx, ny, flag], visited, dist + 1)
+                dfs(nx, ny, dist + 1)
                 visited[nx][ny] = False
 
-    for point in points:
-        visited = [[False] * N for _ in range(N)]
-        dfs(point, visited, 1)
+    for x, y in points:
+        # 시작 좌표 방문처리 제대로 해주기..!
+        visited[x][y] = True
+        dfs(x, y, 1)
+        visited[x][y] = False
 
     print(f"#{tc} {answer}")
-
-    # 무선, 원자, 벽돌, 보호필름
