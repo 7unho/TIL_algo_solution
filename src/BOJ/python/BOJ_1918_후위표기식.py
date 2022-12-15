12f2d110d5dd96328f4eac37f76b2bcf0eb1be98
7unho <rlawnsgh8395@naver.com>
2022-12-16 01:10:30 +0900

    [ADD] BOJ_1918 solved

--- src/BOJ/python/BOJ_1918_후위표기식.py
+++ src/BOJ/python/BOJ_1918_후위표기식.py
@@ -3,9 +3,40 @@
 import sys
 input = sys.stdin.readline
 
-inorder = list('(' + input().rstrip() + ')')
-stack = []
+inorder = list(input().rstrip() + ')')
+stack = ['(']
 answer = []
 
-print(inorder)
+
+def solution(item):
+    global stack, answer
+    while True:
+        top = stack[-1]
+
+        if top == '(':
+            if item == ')': stack.pop()
+            else: stack.append(item)
+
+            return
+        
+        if item in ('*', '/') and top in ('+', '-'):
+            stack.append(item)
+            return
+        
+        answer.append(stack.pop())
+
+for item in inorder:
+    if item.isalpha():
+        answer.append(item)
+        continue
+
+    top = stack[-1]
+    # ')'를 받을 때마다, 스택을 정리.
+    if item == ')' or item in ('+', '-') or (item in ('*', '/') and top in ('*', '/')): 
+        solution(item)
+        continue
+
+    stack.append(item)
+
+print(''.join(answer))
 
