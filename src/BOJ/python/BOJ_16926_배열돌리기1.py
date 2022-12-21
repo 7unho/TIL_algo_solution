fc13818a9a346b5ad9d5c21b78769ebc30e80b6f
7unho <rlawnsgh8395@naver.com>
2022-12-22 00:44:50 +0900

    [ADD] BOJ_16926 solved

--- src/BOJ/python/BOJ_16926_배열돌리기1.py
+++ src/BOJ/python/BOJ_16926_배열돌리기1.py
@@ -1,14 +1,38 @@
-# import sys
-# input = sys.stdin.readline
+import sys
+input = sys.stdin.readline
 
-# # 입력값 : N(행), M(열), R(회전 수)
-
-# N, M, R = map(int, input().split())
-
-# graph = [list(map(int, input().split())) for _ in range(N)]
-
-K = 7
+# 입력값 : N(행), M(열), R(회전 수)
+X, Y = 0, 1
+N, M, R = map(int, input().split())
+depth = min(N, M) // 2
+graph = [list(map(int, input().split())) for _ in range(N)]
 
 
-for i in range(2, 100):
-    print((i + 1) // 2)
+def rotate(start, end):
+    global graph
+    temp = graph[start[X]][start[Y]]
+    
+    # ⬅️ 방향
+    for i in range(start[Y], end[Y]):
+        graph[start[X]][i] = graph[start[X]][i + 1]
+
+    # ⬆️ 방향
+    for i in range(start[X], end[X]):
+        graph[i][end[Y]] = graph[i + 1][end[Y]]
+    
+    # ➡️ 방향
+    for i in range(end[Y], start[Y], -1):
+        graph[end[X]][i] = graph[end[X]][i - 1]
+    
+    # ⬇️ 방향
+    for i in range(end[X], start[X] + 1, -1):
+        graph[i][start[Y]] = graph[i - 1][start[Y]]
+    
+    graph[start[X] + 1][start[Y]] = temp
+
+for _ in range(R):
+    for i in range(depth):
+        rotate((0 + i, 0 + i), (N - 1 - i, M - 1 - i))
+
+for i in range(N):
+    print(*graph[i])
