c9f1abf0b5dad933e117f25278bfd76f1bb37e57
7unho <71058075+7unho@users.noreply.github.com>
2023-06-21 23:38:18 +0900

    [unrated] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -0,0 +1,38 @@
+-- SELECT HISTORY_ID, FEE
+-- ORDER BY FEE desc, HISTORY_ID desc;
+
+WITH HISTORY_WITH_CAR_INFO AS
+(
+    SELECT
+        history_id,
+        c.car_id,
+        c.car_type,
+        c.daily_fee,
+        datediff(end_date, start_date) + 1 'period',
+        CASE
+            WHEN datediff(end_date, start_date) + 1 < 7
+            THEN NULL
+            WHEN datediff(end_date, start_date) + 1 < 30
+            THEN '7일 이상'
+            WHEN datediff(end_date, start_date) + 1 < 90
+            THEN '30일 이상'
+            ELSE '90일 이상'
+        END 'duration_type'
+    FROM
+        CAR_RENTAL_COMPANY_CAR c,
+        CAR_RENTAL_COMPANY_RENTAL_HISTORY h
+    WHERE
+        c.car_id = h.car_id
+        and car_type = '트럭'
+)
+
+# select * from history_with_car_info;
+SELECT
+    history_id,
+    (daily_fee * ( 100 - ifnull(discount_rate, 0)) div 100 ) * period 'FEE'
+FROM
+    HISTORY_WITH_CAR_INFO T1 LEFT OUTER JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN T2 
+    ON T1.car_type = T2.car_type and T1.duration_type = T2.duration_type
+ORDER BY fee desc, history_id desc;
+
+# select * from car_rental_company_car;
