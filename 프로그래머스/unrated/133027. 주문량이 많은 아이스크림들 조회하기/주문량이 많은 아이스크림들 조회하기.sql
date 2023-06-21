27cb8f827e8b3bb6334d7f3781f06edd04ecdad1
7unho <71058075+7unho@users.noreply.github.com>
2023-06-21 23:59:23 +0900

    Update 주문량이 많은 아이스크림들 조회하기.sql

--- 프로그래머스/unrated/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/unrated/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,13 +1,13 @@
 -- 7월 총 주문량 + 상반기 아이스크림 상위 3개의 맛
 -- SELECT FLAVOR
 -- ORDER BY 총 주문량 desc
-SELECT flavor
+SELECT FLAVOR
 FROM
-(    SELECT j.shipment_id, j.flavor, sum((j.total_order + ifnull(f.total_order, 0))) 'total_count'
+(    
+    SELECT j.shipment_id, j.flavor, sum((j.total_order + ifnull(f.total_order, 0))) 'total_count'
     FROM
-        july j left outer join
-        first_half f on f.shipment_id = j.shipment_id
-    group by j.flavor
-    order by total_count desc limit 3) `solution`; # 초코, 딸기, 화이트초코
+        july j LEFT OUTER JOIN first_half f ON f.shipment_id = j.shipment_id
+    GROUP BY j.flavor
+    ORDER BY total_count DESC LIMIT 3
+) `solution`;
     
-
