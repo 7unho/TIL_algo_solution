5b6834e29633b5f576858e99c819c94301e7e15f
7unho <71058075+7unho@users.noreply.github.com>
2023-06-22 20:39:17 +0900

    [unrated] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -0,0 +1,17 @@
+-- 2022년 4월 13일 취소되지 않은 CS 진료 내역 조회
+-- SELECT APNT_NO, PT_NAME, PT_NO, MCDP_CD, DR_NAME, APNT_YMD
+-- ORDER BY APNT_YMD;
+
+with `T1` as (
+SELECT apnt_ymd, apnt_no, pt_no, mddr_id, mcdp_cd
+FROM appointment
+where apnt_ymd like '2022-04-13%' and apnt_cncl_yn = 'N' and mcdp_cd = 'cs')
+
+select t1.apnt_no, p.pt_name, t1.pt_no, t1.mcdp_cd, d.dr_name, t1.apnt_ymd
+from 
+    t1,
+    patient p,
+    doctor d
+where t1.pt_no = p.pt_no
+and d.dr_id = t1.mddr_id
+order by t1.apnt_ymd;
