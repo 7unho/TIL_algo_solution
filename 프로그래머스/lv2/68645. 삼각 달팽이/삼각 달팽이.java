b86a85ad02dede620f129cec3d442cbeedd9bfa1
7unho <71058075+7unho@users.noreply.github.com>
2023-06-29 15:59:17 +0900

    [level 2] Title: 삼각 달팽이, Time: 42.89 ms, Memory: 161 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    private static final int[] dx = {1, 0, -1};
+    private static final int[] dy = {0, 1, -1};
+    
+    public int[] solution(int n) {
+        List<Integer> answer = new ArrayList<>();
+        int num = 2;
+        int x = 0;
+        int y = 0;
+        
+        int[][] arr = new int[n][n];
+        arr[0][0] = 1;
+        
+        for(int i = 0; i < n; i++){
+            int direction = i % 3;
+            
+            while(true){
+                int nx = x + dx[direction];
+                int ny = y + dy[direction];
+                
+                if(nx < 0 || nx >= n || ny < 0 || ny > nx) break;
+                if(arr[nx][ny] != 0) break;
+                
+                arr[nx][ny] = num;
+                num += 1;
+                x = nx;
+                y = ny;
+            }
+        }
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j <= i; j++){
+                answer.add(arr[i][j]);
+            }
+        }
+        
+        return answer.stream().mapToInt(i -> i).toArray();
+    }
+}
