7c47e369c6e1847c7e9d1c4c291ff653ed68c927
7unho <71058075+7unho@users.noreply.github.com>
2023-08-17 18:31:37 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.06 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -5,19 +5,7 @@ import java.util.stream.IntStream;
 // 2. 3진수 뒤집기
 // 3. 3진수 -> 10진수
 class Solution {
-    public String step2(char[] step1){
-        return IntStream.range(0, step1.length)
-                        .mapToObj(i -> step1[step1.length - 1 - i])
-                        .map(Object::toString)
-                        .collect(Collectors.joining());
-    }
-    public int solution(int n) {        
-        // 1. 10 -> 3진수
-        String step1 = Integer.toString(n, 3);
-        
-        // 2. 3진수 뒤집기
-        String step2 = step2(step1.toCharArray());
-        
-        return Integer.parseInt(step2, 3);
+    public int solution(int n) {
+        return Integer.parseInt(new StringBuilder(Integer.toString(n, 3)).reverse().toString(), 3);
     }
 }
