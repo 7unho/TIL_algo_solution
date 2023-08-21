43deb04835c40c42ca7d459427a1e1b8ddffa9c9
7unho <71058075+7unho@users.noreply.github.com>
2023-08-22 01:43:29 +0900

    [level 2] Title: 하노이의 탑, Time: 1.87 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/lv2/12946. 하노이의 탑/README.md
+++ 프로그래머스/lv2/12946. 하노이의 탑/README.md
@@ -1,10 +1,10 @@
 # [level 2] 하노이의 탑 - 12946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12946) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12946?language=java) 
 
 ### 성능 요약
 
-메모리: 136 MB, 시간: 12.21 ms
+메모리: 112 MB, 시간: 1.87 ms
 
 ### 구분
 
--- 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.java
@@ -1,23 +1,26 @@
 import java.util.*;
-// N개의 원반을 현재(current) 기둥에서 목표 기둥(next)로 옮기는 문제
-// (n, currrent, next)
+
+// N개의 원반을 현재 기둥에서 목표 기둥으로 옮기는 문제
+// (n, current, target)
 class Solution {
     public List<int[]> solution(int n) {
-        return hanoi(n, 1, 3);
+        List<int[]> answer = new ArrayList<int[]>();
+        hanoi(n, 1, 3, answer);
+        return answer;
     }
     
-    public List<int[]> hanoi(int n, int current, int next){
-        // 1. 종료조건
-        if(n == 1) return List.of(new int[] {current, next});
+    public void hanoi(int n, int current, int target, List<int[]> answer){
+        if(n == 1){
+            answer.add(new int[] {current, target});
+            return;
+        }
         
-        int empty = 6 - (current + next);
+        // empty = 빈 기둥
+        int empty = 6 - (current + target);
         
-        // (n, current, next) = (n - 1, current, empty) + (1, current, next) + (n - 1, empty, next)
-        List<int[]> answer = new ArrayList<int[]>();
-        answer.addAll(hanoi(n - 1, current, empty));
-        answer.addAll(hanoi(1, current, next));
-        answer.addAll(hanoi(n - 1, empty, next));
-        
-        return answer;  
+        // (n, current, target) = (n - 1, current, empty) + (1, current, target) + (n - 1, empty, target)
+        hanoi(n - 1, current, empty, answer);
+        hanoi(1, current, target, answer);
+        hanoi(n - 1, empty, target, answer);
     }
 }
