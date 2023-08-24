9807c0a2f24ddcb6be5a6d896e1fd02b87b337fe
7unho <71058075+7unho@users.noreply.github.com>
2023-08-24 14:52:38 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.96 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -1,56 +1,58 @@
 import java.util.*;
+
 class Solution {
     static String[][] priority = {
         {"*", "-", "+"},
         {"*", "+", "-"},
-        {"+", "*", "-"},
         {"+", "-", "*"},
-        {"-", "*", "+"},
-        {"-", "+", "*"}
+        {"+", "*", "-"},
+        {"-", "+", "*"},
+        {"-", "*", "+"}
     };
     
-    public long calculator(long a, long b, String op){
+    public long solution(String expression) {
+        StringTokenizer st = new StringTokenizer(expression, "*+-", true);
+        List<String> tokens = new ArrayList<>();
+        
+        while(st.hasMoreTokens()) tokens.add(st.nextToken());
+        
+        long max = Long.MIN_VALUE;
+        
+        for(String[] opList: priority){
+            long CASE = Math.abs(calculator(new ArrayList<>(tokens), opList));
+            
+            if(max < CASE) max = CASE;
+        }
+        return max;
+    }
+    
+    public long calculator(long lNum, long rNum, String op){
         return switch(op){
-            case "*" -> a * b;
-            case "+" -> a + b;
-            case "-" -> a - b;
-            default -> 0;
+                case "*" -> lNum * rNum;
+                case "+" -> lNum + rNum;
+                case "-" -> lNum - rNum;
+                default -> 0;
         };
     }
     
-    public long calculator(List<String> tokens, String[] priority){
-        for(String op: priority){
+    public long calculator(List<String> tokens, String[] opList){
+        for(String op: opList){
             for(int i = 0; i < tokens.size(); i++){
                 if(!op.equals(tokens.get(i))) continue;
                 
-                long a = Long.parseLong(tokens.get(i - 1));
-                long b = Long.parseLong(tokens.get(i + 1));
+                long lNum = Long.parseLong(tokens.get(i - 1));
+                long rNum = Long.parseLong(tokens.get(i + 1));
+                long sum = calculator(lNum, rNum, op);
                 
-                long res = calculator(a, b, op);
+                tokens.remove(i - 1);
+                tokens.remove(i - 1);
+                tokens.remove(i - 1);
+                tokens.add(i - 1, String.valueOf(sum));
                 
-                tokens.remove(i - 1);
-                tokens.remove(i - 1);
-                tokens.remove(i - 1);
-                tokens.add(i - 1, String.valueOf(res));
                 i -= 2;
             }
         }
         
-        return Math.abs(Long.parseLong(tokens.get(0)));
-    }
-    public long solution(String expression) {
-        StringTokenizer st = new StringTokenizer(expression, "*-+", true);
-        List<String> tokens = new ArrayList<>();
-        while(st.hasMoreTokens()){
-            tokens.add(st.nextToken());
-        }
-        
-        long max = 0;
-        for(String[] p: priority){
-            long value = calculator(new ArrayList<>(tokens), p);
-            if(max < value) max = value;
-        }
-        
-        return max;
+        return Long.parseLong(tokens.get(0));
     }
 }
