d5b395ceb36b11798c1d7930a1b7362bd5e44812
7unho <71058075+7unho@users.noreply.github.com>
2023-08-26 10:07:54 +0900

    [level 2] Title: 소수 찾기, Time: 2.29 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/README.md
+++ 프로그래머스/lv2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=java) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.54 ms
+메모리: 79.1 MB, 시간: 2.29 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -5,14 +5,14 @@ class Solution {
     static int N;
     public int solution(String nums) {
         Set<Integer> primes = new HashSet<>();
-        int[] numbers = nums.chars().map(c -> c - '0').toArray();
+        int[] numbers = nums.chars().map(n -> n - '0').toArray();
         N = numbers.length;
-        getPrimes(0, numbers, new boolean[N], primes);
+        subset(0, numbers, new boolean[N], primes);
         return primes.size();
     }
     
     public boolean isPrime(int number){
-        if(number < 2) return false;
+        if(number <= 1) return false;
         if(number == 2 || number == 3) return true;
         for(int i = 2; i < Math.sqrt(number) + 1; i++){
             if(number % i == 0) return false;
@@ -20,15 +20,15 @@ class Solution {
         return true;
     }
     
-    public void getPrimes(int number, int[] numbers, boolean[] isSelected, Set<Integer> primes){
+    public void subset(int number, int[] numbers, boolean[] isSelected, Set<Integer> primes){
         if(isPrime(number)) primes.add(number);
         
-        for(int i = 0; i < N; i++){
+        for(int i = 0; i < numbers.length; i++){
             if(isSelected[i]) continue;
             int next = number * 10 + numbers[i];
             
             isSelected[i] = true;
-            getPrimes(next, numbers, isSelected, primes);
+            subset(next, numbers, isSelected, primes);
             isSelected[i] = false;
         }
     }
