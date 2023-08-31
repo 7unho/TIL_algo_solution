fcfeca89f70199ed8734fe6c6c395eef9b980a5c
7unho <71058075+7unho@users.noreply.github.com>
2023-08-31 12:13:02 +0900

    [level 2] Title: 가장 큰 수, Time: 3.89 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        return Arrays.stream(numbers).mapToObj(String::valueOf).sorted((v1, v2) -> {
+            int original = Integer.parseInt(v1 + v2);
+            int reversed = Integer.parseInt(v2 + v1);
+            return reversed - original;
+        }).collect(Collectors.joining("")).replaceAll("^0+", "0");
+    }
+}
