cd70ec924a70e4e36af6e2648f6263860bc48add
7unho <71058075+7unho@users.noreply.github.com>
2023-09-06 21:16:30 +0900

    [Silver III] Title: 1로 만들기, Time: 108 ms, Memory: 15860 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        if(N == 1) {
+            System.out.println(0);
+            System.exit(0);
+        }
+        int[] dp = new int[N + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[1] = 0;
+        dp[2] = 1;
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = dp[i - 1] + 1;
+
+            if(i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            if(i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+        }
+
+        System.out.println(dp[N]);
+    }
+}
