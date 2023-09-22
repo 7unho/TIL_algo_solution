2cdb4575ca2386805a271ca0bf20cc2597856104
7unho <71058075+7unho@users.noreply.github.com>
2023-09-22 16:50:03 +0900

    [level 2] Title: 구명보트, Time: 12.75 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.py
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -1,32 +1,16 @@
-from collections import deque
-
 def solution(people, limit):
     people.sort()
-    people = deque(people)
-    answer = 0
-
+    
     start = 0    
     end = len(people) - 1
+    answer = 0
+    
     while True:
-        if not people: break
-        _sum = people[start] + people[end] if start != end else people[start]
+        if start >= end: break
         
-        if _sum <= limit:
+        if people[start] + people[end] <= limit:
+            start += 1
             answer += 1
-            if start == end:
-                people.popleft()
-            else:
-                people.pop()
-                people.popleft()
+        end -= 1
             
-            start = 0
-            end = len(people) - 1
-            continue
-            
-        if _sum > limit:
-            people.pop()
-            answer += 1
-            end -= 1
-            continue
-            
-    return answer
+    return len(people) - answer
