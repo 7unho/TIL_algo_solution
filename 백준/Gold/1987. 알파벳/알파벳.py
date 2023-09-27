8bb3f7be12177c656a8eae2a476a0bfa59e7367c
7unho <71058075+7unho@users.noreply.github.com>
2023-09-27 20:28:50 +0900

    [Gold IV] Title: 알파벳, Time: 6644 ms, Memory: 173944 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -3,27 +3,30 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split())
 graph = [list(input().rstrip()) for _ in range(N)]
-items = set()
+visited = [False] * 26
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
 answer = 1
-def process(x, y):
+visited[ord(graph[0][0]) - 65] = True
+
+def process(x, y, items):
     global answer
 
-    q = set([(x, y, graph[x][y])])
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
 
-    while q:
-        x, y, items = q.pop()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+        if not (0 <= nx < N and 0 <= ny < M): continue
+        item = ord(graph[nx][ny]) - 65
+        if visited[item]: continue
+        visited[item] = True
+        items.add(item)
+        answer = max(answer, len(items))
+        process(nx, ny, items)
+        visited[item] = False
+        items.remove(item)
+        
 
-            if not (0 <= nx < N and 0 <= ny < M): continue
-            if graph[nx][ny] in items: continue
-            q.add((nx, ny, items + graph[nx][ny]))
-            answer = max(answer, len(items) + 1)
-
-
-process(0, 0)
+process(0, 0, {ord(graph[0][0]) - 65})
 
 print(answer)
