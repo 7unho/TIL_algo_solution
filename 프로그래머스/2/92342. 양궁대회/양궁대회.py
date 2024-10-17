6b5b2cf923f12a4a26eabdc1928389abec383691
7unho <71058075+7unho@users.noreply.github.com>
2024-10-18 07:46:20 +0900

    [level 2] Title: 양궁대회, Time: 4.34 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.py
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.py
@@ -4,23 +4,21 @@ result := 라이언이 가장 큰 점수 차로 이길 경우의 수
 """
 from itertools import product
 def solution(n, info):
-    info.reverse()
     answer = [-1]
-    maxGap = 0
+    scoreGap = 0
     for case in product((True, False), repeat = 11):
         cnt = sum([info[i] + 1 for i in range(11) if case[i]])
         
         if cnt > n: continue
-        apeech = sum([i for i in range(11) if info[i] > 0 and not case[i]])        
-        ryan = sum([i for i in range(11) if case[i]])
+        apeech = sum([10 - i for i in range(10) if info[i] > 0 and not case[i]])        
+        ryan = sum([10 - i for i in range(10) if case[i]])
         
-        if maxGap >= ryan - apeech: continue
-           
-        maxGap = ryan - apeech
-        answer = [info[i] + 1 if case[i] else 0 for i in range(11)]
-        answer[0] += n - cnt
-        
-    answer.reverse()
+        if scoreGap < ryan - apeech:
+            scoreGap = ryan - apeech
+            print(scoreGap, ryan, apeech)
+            answer = [info[i] + 1 if case[i] else 0 for i in range(11)]
+            answer[-1] += n - cnt
+
     return answer
 
     
