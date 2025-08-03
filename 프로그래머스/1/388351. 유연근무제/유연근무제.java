ba22b5dbb790d20a669e06d8ac38081d6ec3567d
7unho <71058075+7unho@users.noreply.github.com>
2025-08-03 10:24:41 +0900

    [level 1] Title: 유연근무제, Time: 0.75 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.java
@@ -0,0 +1,49 @@
+/**
+n := 직원 수
+schedules := 출근 희망 시각
+timelogs := 일주일 실 출근 시각, hour * 100 + minute
+startday := 시작 요일 1(월) ~ 7(일)
+
+
+*/
+class Solution {
+    public int solution(int[] schedules, int[][] timelogs, int startday) {
+        int answer = 0;
+        int N = schedules.length;
+        
+        for (int crew = 0; crew < N; crew++) {
+            if (isSuccess(schedules[crew], timelogs[crew], startday, crew)) {
+                answer += 1;
+            }
+        }
+        return answer;
+    }
+    
+    public boolean isSuccess(int schedule, int[] timelogs, int startday, int crew) {
+        for (int day = 0; day < 7; day++) {
+            if (isIgnored(startday, day + 1)) continue;
+            if (isFailed(schedule, timelogs[day])) return false;
+        }
+        return true;
+    }
+    
+    public boolean isIgnored(int startday, int day) {
+        if (startday == 7) {
+            return day == 1 || day == 7;
+        }
+        return 8 - startday == day || 7 -  startday == day;
+    }
+    
+    public boolean isFailed(int schedule, int timelog) {
+        int hour = schedule / 100;
+        int minute = schedule % 100;
+        
+        if (minute + 10 >= 60) {
+            hour += 1;
+            minute = (minute + 10) % 60;
+        } else {
+            minute += 10;
+        }
+        return hour * 100 + minute < timelog;
+    }
+}
